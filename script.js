d3351e32972e04f1defec7992b936cf8d21ed6d8
KakKakowski <127879619+KakKakowski@users.noreply.github.com>
2024-01-17 20:10:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const stripe = Stripe('pk_live_51OZVlaGgga06qK7VzhPiLe8FFukJTBj93yXQqmszMBDxS93H4GHvFj3bFFCNEtmbgxUomIECirBElO9ESNWaV5fC00KZVkW0Gy');
-const elements = stripe.elements();
+const adviceParagraph = document.getElementById('adviceParagraph');
+const unlockButton = document.getElementById('unlockButton');
+const drawButton = document.getElementById('drawButton');
 
-// Pobierz elementy DOM
-const adviceParagraph = document.querySelector('.wrapped-text');
-
-// Lista z radami
 const adviceList = [
   "Zawsze dbaj o równowagę między ambitnymi marzeniami, a umiejętnością cieszenia się chwilą.",
   "Podziwiaj piękno teraźniejszości, zamiast ciągle pędzić do przodu.",
@@ -13,32 +10,21 @@ const adviceList = [
   "Szukaj harmonii między pracą a życiem prywatnym."
 ];
 
-// Funkcja do obsługi odblokowania
 function unlock() {
   // Tutaj dodaj kod obsługujący płatność, np. wywołanie płatności za pomocą Stripe
   // Po pomyślnej płatności możesz ukryć przycisk "Odblokuj" i pokazać przycisk "Losuj"
-  document.getElementById('unlockButton').style.display = 'none';
-  document.getElementById('drawButton').style.display = 'block';
+  unlockButton.style.display = 'none';
+  drawButton.style.display = 'block';
 }
 
-// Funkcja do obsługi losowania
 function draw() {
-  // Tutaj dodaj kod obsługujący losowanie
-  // Po pomyślnym losowaniu możesz ukryć przycisk "Losuj" i pokazać przycisk "Odblokuj"
-  document.getElementById('drawButton').style.display = 'none';
-  document.getElementById('unlockButton').style.display = 'block';
-}
-
-
-// Funkcja losująca radę i aktualizująca tekst
-function getRandomAdvice() {
   const randomIndex = Math.floor(Math.random() * adviceList.length);
-  const randomAdvice = adviceList[randomIndex];
-  const adviceParagraph = document.getElementById('adviceParagraph');
-  adviceParagraph.textContent = randomAdvice;
+  adviceParagraph.textContent = adviceList[randomIndex];
+
+  // Ukrycie przycisku "Losuj" i pokazanie przycisku "Odblokuj"
+  drawButton.style.display = 'none';
+  unlockButton.style.display = 'block';
 }
 
 // Dodaj nasłuchiwanie na kliknięcie przycisku "Losuj"
-const drawButton = document.getElementById('drawButton');
-drawButton.addEventListener('click', getRandomAdvice);
-
+drawButton.addEventListener('click', draw);
